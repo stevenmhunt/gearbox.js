b0d2dc9c789b2744728f3268e33be2d3573dac42
Steven Hunt <steven.mtw.hunt@gmail.com>
2012-12-21 10:53:10 -0500

    v0.2 release

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 Gearbox.js: Throttle the performance of your Web Application
 ============================================================
 
-**Version 0.1**
+**Version 0.2**
 
-Features
+Overview
 --------
 
 Runs a continuous timing check to estimate delays in the JavaScript event queue.
@@ -11,6 +11,17 @@ Runs a continuous timing check to estimate delays in the JavaScript event queue.
 Provides events to your application about performance impacts in real-time.
 
 Uses the concept of "gears" for communicating what your application should do to accommodate performance problems.
+
+Version History
+---------------
+### Version 0.2
+* Added gearbox.all to perform actions to all gearboxes at once.
+* Added cookie support with gearbox.save(), which stores gear values on the client's machine.
+* Added more sophisticated up and down shifting capabilities.
+
+### Version 0.1
+* Initial release
+* Support for timing mechanism and basic gear response.
 
 How To Use
 ----------
@@ -23,10 +34,15 @@ How To Use
 /* Gearbox Parameters */
 var timing = 1000; //how often to check for delays.
 var delay = 50; //maximum acceptible delay.
+var gearInitial = 1; //the initial gear to use.
 var gearCount = 10; //maximum number of gears.
 
 /* Gearbox Creation */
+var gears = gearbox.create('my gearbox', timing, delay, gearInitial, gearCount);
+
+/* Also Valid... */
 var gears = gearbox.create('my gearbox', timing, delay, gearCount);
+
 ```
 
 Note that these are the default values, so if you do not provide these parameters the gearbox will be configured this way by default.
@@ -55,6 +71,30 @@ gears.shiftDown();
 
 ```
 
-Roadmap
--------
-More sophisticated gear changing mechanisms.
+Advanced
+--------
+
+You can provide more robust shifting rules as follows:
+
+```javascript
+var gears = gearbox.create('my gearbox', 1000, {
+	/* rules to shift up... */
+	increment: [
+		{
+			/* if the delay is above 50 milliseconds */
+			delay_above: 50,
+			/* for one or more timing iterations... */
+			duration: 1
+		}
+	],
+	/* rules to shift down... */
+	decrement: [
+		{
+			/* if the delay is below 10 milliseconds */
+			delay_below: 10,
+			/* for three or more timing iterations... */
+			duration: 3
+		}
+	]
+}, 1, 10);
+```
--- gearbox.js
+++ gearbox.js
@@ -1,28 +1,39 @@
 /* gearbox.js
- * version 0.1
+ * version 0.2
  * written by Steven Hunt
  * released under the MIT License. */
 
 //create the gearbox "namespace".
-var gearbox = gearbox || { version: "0.1" };
+var gearbox = gearbox || { version: "0.2" };
 
 (function(g) {
 	
-	var instance = function(interval, threshold, top) {
+	var instance = function(interval, threshold, initial_gear, top_gear) {
 	
 		//default interval is 1 second.
 		this.interval = interval !== undefined ? interval : 1000;
 		
 		//threshold for changing the gear; default is 50 milliseconds 0f delay.
-		this.threshold = threshold !== undefined ? threshold : 50;
+		if (threshold === undefined)
+			this.threshold = { 'increment': { 'delay_above': 50, 'duration': 1 } };
+		else if (isNaN(threshold))
+			this.threshold = threshold;
+		else
+			this.threshold = { 'increment': { 'delay_above': threshold, 'duration': 1 } }; 
+
+		//handle constructor overloading.
+		if (initial_gear !== undefined && top_gear === undefined) {
+			top_gear = initial_gear;
+			initial_gear = 1;
+		}
 
 		//top gear to use, default is 10.
-		this.top = top !== undefined ? top : 10;
-		
+		this.top = top_gear !== undefined ? top_gear : 10;
+			
 		//initial gear is 1.
-		var gear = 1;
+		var gear = initial_gear !== undefined ? initial_gear : 1;
 		this.currentGear = function() { return gear; };
-				
+		
 		//event handler callbacks.
 		var callbacks = { 'shift': new Array() };
 		
@@ -75,6 +86,7 @@ var gearbox = gearbox || { version: "0.1" };
 		var prevInterval = 0;
 
 		var performance = 0;
+		var duration = 0;
 		
 		//returns the last reported delay (in milliseconds)
 		this.performance = function() {
@@ -82,24 +94,59 @@ var gearbox = gearbox || { version: "0.1" };
 		};
 		
 		//indicates the current status of the gearbox.
-		this.status = "started";
+		this.status = "stopped";
 		
 		var that = this;
 		
-		var intervalCheck = null;
+		//calculate whether to change gears.
+		var checkPerformance = function() {
+			
+			var items = [that.threshold.increment instanceof Array ? that.threshold.increment : new Array(that.threshold.increment),
+						 that.threshold.decrement instanceof Array ? that.threshold.decrement : new Array(that.threshold.decrement)];			
+			
+			for (var i = 0; i < items.length; i++) {
+				
+				if (items[i] === undefined)
+					continue;
+					
+				for (var j = 0; j < items[i].length; j++) {
+
+					var c = items[i][j];
+
+					if (c === undefined)
+						continue;
+
+					if ((c.delay_above && performance > c.delay_above) || (c.delay_below && performance < c.delay_below)) {
+						
+						duration++;
+						
+						if (!c.duration || duration >= c.duration) {
+							if (i == 0)
+								that.shiftUp();
+							else
+								that.shiftDown();
+						}
+						
+						return;
+					}
+				}
+			}
+		};
+		
+		//handle timeouts
+		var intervalCheck = null;		
 		var timeout = function() {
 			if (that.status == "stopped")
 				return;
 			setTimeout(intervalCheck, that.interval);
 		};
 		
+		//calculate performance and check it.
 		intervalCheck = function() {
 			var currTime = new Date().getTime();
 			if (prevInterval > 0) {
 				performance = currTime - prevInterval - that.interval;
-				if (performance > that.threshold) {
-					that.shiftUp();
-				}
+				checkPerformance();
 			}
 			prevInterval = currTime;
 			timeout();
@@ -125,7 +172,31 @@ var gearbox = gearbox || { version: "0.1" };
 	};
 	
 	//track all gearboxes running on the page.
-	instances = { "default": new instance() };
+	instances = { };
+
+	//reads a cookie by name.
+	var readCookie = function(name) {
+		
+		var cookies = document.cookie.split('; ');
+		for (var i = 0; i < cookies.length; i++) {
+			var cookie = cookies[i].split('=');
+			if (unescape(cookie[0]) == name)
+				return unescape(cookie[1]);
+		}
+		
+		return null;
+	};
+
+	//writes a cookie.
+	var writeCookie = function(name, value, exdays) {
+	
+		var exdate = new Date();
+		exdate.setDate(exdate.getDate() + exdays);
+		var c_name = escape(name);
+		var c_value = escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+		document.cookie= c_name + "=" + c_value;
+
+	};
 	
 	//used to get a specific gearbox.
 	g.get = function(name) {
@@ -135,15 +206,24 @@ var gearbox = gearbox || { version: "0.1" };
 	};
 	
 	//used to create a new gearbox.
-	g.create = function(name, interval, threshold, top) {
+	g.create = function(name, interval, threshold, initial, top) {
 		
 		//name already exists!
 		if (instances.hasOwnProperty(name))
 			return false;
-			
-		instances[name] = new instance(interval, threshold, top);
+		
+		//check for cookie.
+		var cookie = readCookie("gearbox_js_"+name);
+		if (cookie)
+			initial = cookie;
+		
+		instances[name] = new instance(interval, threshold, initial, top);
+				
 		return instances[name];
 	};
+	
+	//create a default gearbox.
+	g.create("default");
 	
 	//used to destroy a gearbox.
 	g.destroy = function(name) {
@@ -157,6 +237,57 @@ var gearbox = gearbox || { version: "0.1" };
 		delete instances[name];
 	
 		return true;
+	};
+		
+	//stores the value of the current gear in a cookie.
+	g.save = function(name) {
+		if (name === undefined) {
+			var keys = Object.keys(instances);
+			for (var i = 0; i < keys.length; i++) {
+				writeCookie("gearbox_js_"+keys[i], instances[keys[i]].currentGear(), 7);
+			}
+		}
+		else if (instances.hasOwnProperty(name)) {
+				writeCookie("gearbox_js_"+name, instances[name].currentGear(), 7);
+		}
+	}
+	
+	//calls the name function on all instances.
+	var callFnAll = function(name) {
+		var result = {};
+		var keys = Object.keys(instances);
+		for (var i = 0; i < keys.length; i++) {
+			var instance = instances[keys[i]];
+			result[keys[i]] = instance[name]();
+		}
+		return result;
+	};
+	
+	g.all = {
+		
+		start: function() {
+			return callFnAll("start");
+		},
+		
+		stop: function() {
+			return callFnAll("stop");
+		},
+		
+		shiftUp: function() {
+			return callFnAll("shiftUp");
+		},
+		
+		shiftDown: function() {
+			return callFnAll("shiftDown");
+		},
+		
+		performance: function() {
+			return callFnAll("performance");
+		},
+
+		currentGear: function() {
+			return callFnAll("currentGear");
+		}		
 	};
 	
 })(gearbox);
